cd6513cf95cd9ecdc3009b8f062212631ef61148
deno101 <dennizwarui@gmail.com>
2020-07-07 16:42:21 +0300

    add navigation links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -19,12 +19,17 @@ class Navbar extends Component {
         });
     }
 
+    search = () => {
+        // make http request to server search
+        console.log("Search")
+    }
+
     render() {
         return (
             // main container all
             <div>
-                {/* continer img, search, icons, alt-links, slogan */}
                 <Flag />
+                {/* continer img, search, icons, alt-links, slogan menus*/}
                 <div className="container-img-search" >
                     {/* image container */}
                     <div className=" m-2 ml-5" > <Img /> </div>
@@ -37,9 +42,14 @@ class Navbar extends Component {
                             <Socials />
                         </div>
                         {/* search bar */}
-                        <div className="searchbar mt-4">
-                            <SearchBar onChange={this.onChange} />
+                        <div className="searchbar mt-2">
+                            <SearchBar onChange={this.onChange} search={this.search} />
                         </div>
+                    </div>
+
+                    {/* menus */}
+                    <div className="m-nv-menu mr-4">
+                        <Menus />
                     </div>
 
                 </div>
@@ -50,15 +60,15 @@ class Navbar extends Component {
 
 function Img() {
     return (
-        <div>
+        <React.Fragment>
             <img className="Img" src={logo} alt="logo" />
-        </div>
+        </React.Fragment>
     );
 }
 
 function SloganLinks() {
     return (
-        <div className="slogan-links m-2">
+        <div className="slogan-links m-2 mr-4">
             {/* slogan */}
             <div className="mr-4" style={{ color: '#AF9119', fontStyle: "italic" }} >The Kenya Tourism Board welcomes you to Magical Kenya!</div>
             {/* link */}
@@ -106,8 +116,8 @@ function Flag() {
     let colors = ["red", "black", "green"];
     return (
         <div className="flag mb-3" >
-            {colors.map(color => (
-                <div style={{ background: color }}> &nbsp;</div>
+            {colors.map((color, index) => (
+                <div key={index} style={{ background: color }}> &nbsp;</div>
             ))}
         </div>
     );
@@ -117,10 +127,94 @@ function SearchBar(props) {
     return (
         <React.Fragment>
             <input type="text" onChange={props.onChange} placeholder="Search..." />
-            <i className="fa fa-search" ></i>
+            <i className="fa fa-search" onClick={props.search} ></i>
         </React.Fragment>
     );
 }
 
+
+function Menus() {
+    const menu_data = [
+        {
+            name: "What To Do", href: "http://magicalkenya.com/what-to-do", children: [
+                { name: "Go On Safari", href: "" },
+                { name: "Water Sports", href: "" },
+                { name: "Play Golf", href: "" },
+                { name: "Active Travel", href: "" },
+                { name: "Special Interests", href: "" },
+                { name: "Events & Festivals", href: "" },
+                { name: "Culture & Heritage", href: "" },
+                { name: "Entertainment & NightLife", href: "" },
+            ]
+        },
+
+        {
+            name: "What To See", href: "http://magicalkenya.com/what-to-see", children: [
+                { name: "Musemus & Monuments", href: "" },
+                { name: "Porpular Sites", href: "" },
+                { name: "Marine Parks", href: "" },
+                { name: "Conservancies", href: "" },
+                { name: "Rock Art", href: "" },
+            ]
+        },
+
+        {
+            name: "Places To Visit", href: "http://magicalkenya.com/places-to-visit", children: [
+                { name: "Northern Kenya", href: "" },
+                { name: "Wilderness Areas", href: "" },
+                { name: "Coastal Kenya", href: "" },
+                { name: "Cities", href: "" },
+                { name: "HighLand & Valleys", href: "" },
+                { name: "Lakes", href: "" },
+                { name: "Forests", href: "" },
+                { name: "Shopping", href: "" },
+            ]
+        },
+
+        {
+            name: "Visit Kenya", href: "http://magicalkenya.com/visit-kenya", children: [
+                { name: "Visa Information", href: "" },
+                { name: "Getting There", href: "" },
+                { name: "Plan Your Trip", href: "" },
+                { name: "News Update", href: "" },
+                { name: "Sesonal Offers", href: "" },
+                { name: "Learn Swahili", href: "" },
+                { name: "Tour Operators", href: "" },
+                { name: "Why I Love Kenya Magazine", href: "" },
+            ]
+        },
+
+        { name: "Talk To Us", href: "http://magicalkenya/talk-to-us", children: [] },
+
+        { name: "Information On COVID-19", href: "http://magicalkenya/covid-19", children: [] },
+    ]
+    return (
+        <React.Fragment>
+            {menu_data.map(((row, index) => (
+
+                <div className={`m-2 m-nv-menuitem ${index === 0 && 'ml-auto'}`}>
+                    <a href={row.href}>
+                        {row.name}
+                        <i className="fa fa-chevron-down m-1" style={{ color: 'red' }}></i>
+                    </a>
+                    <div className="m-nv-bottom-of-menu">
+                        &nbsp;
+                    </div>
+                    <div>
+                        <div className="m-nv-submenu">
+                            {row.children.map((child, index) => (
+                                <div className={`m-nv-submenuitem pl-3 pr-3 pt-2 pb-2 ${index !== 0 && 'm-nv-submenuitem-bordertop'}`}>
+                                    <a href={child.href} >
+                                        {child.name}
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )))}
+        </React.Fragment>
+    );
+}
 
 export default Navbar;
