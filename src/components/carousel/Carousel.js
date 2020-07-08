ede058a5cd1210c6b7fc5d7624c67164cc88272e
deno101 <dennizwarui@gmail.com>
2020-07-08 20:38:33 +0300

    add responsive js for slider

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,64 +1,91 @@
-import { sss } from './slider-logic';
+import { setTimer } from './slider-logic';
 import React, { PureComponent } from 'react'
 import './Carousel.css';
 
 export default class Carousel extends PureComponent {
     render() {
         return (
-            <div>
-                <div>
-                    <Imgs />
-                </div>
+            <div className="carousel-container">
+                <Imgs />
             </div>
         )
     }
 
     componentDidMount() {
         // call methods to move slider
-        sss();
+        setTimer();
     }
 }
 
 
 function Imgs() {
     const images = [
-        { src: "01.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "02.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "03.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "04.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "05.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "06.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "07.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "08.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "09.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "10.jpg", subcontent: { header: "", mesage: "" } },
-        { src: "11.png", subcontent: { header: "", mesage: "" } },
+        {
+            src: "01.jpg", subcontent: [
+                { header: "Scenery & Landscape", mesage: "Enjoy the breathtaking scenery and magnificent landscape" }
+            ]
+        },
+        {
+            src: "02.jpg", subcontent: [
+                { header: "See The Migration", mesage: "Experience one of the greatest natural spectacles in the world" }
+            ]
+        },
+        {
+            src: "03.jpg", subcontent: [
+                { header: "Water Sports", mesage: "Make some waves on your next vacation" }
+            ]
+        },
+        { src: "04.jpg", subcontent: [] },
+        {
+            src: "05.jpg", subcontent: [
+                { header: "Massai Mara", mesage: "Witness the essence of Wildlife!" }
+            ]
+        },
+        { src: "06.jpg", subcontent: [] },
+        {
+            src: "07.jpg", subcontent: [
+                { header: "White Water Raffing", mesage: "Decend the rapids and experience the thrill" }
+            ]
+        },
+        { src: "08.jpg", subcontent: [] },
+        {
+            src: "09.jpg", subcontent: [
+                { header: "Go on safari", mesage: "Get unbelivebly close to wild animals you've only ever dreamed about" }
+            ]
+        },
+        { src: "10.jpg", subcontent: [] },
+        { src: "11.png", subcontent: [] },
     ]
 
     return (
         <React.Fragment>
-            {images.map((image, index) => (
-                <div key={index} className="carousel-image">
-                    <div className="carousel-image-inner">
-                        <img src={`${process.env.PUBLIC_URL}/imgs/${image.src}`} alt={index}/>
-                    </div>
-                    <div className="carousel-image-content pr-3">
-                        <div className="carousel-image-content-title">
-                            Content
+            <div className="carousel-image-container">
+                {images.map((image, index) => (
+                    <div key={index} className='carousel-image' style={{ opacity: (index === 0) ? '1' : '0' }}>
+                        <div className="carousel-image-inner">
+                            <img src={`${process.env.PUBLIC_URL}/imgs/${image.src}`} alt={index} />
                         </div>
-                        <div className="carousel-image-content-message">
-                            blabla
-                        </div>
+                        {console.log(image.subcontent)}
+                        {image.subcontent.map((data, index) => (
+                            <div className="carousel-image-content pr-3" key={index}>
+                                <div className="carousel-image-content-title m-2 ml-auto">
+                                    {data.header}
+                                </div>
+                                <div className="carousel-image-content-message m-2 ml-auto">
+                                    {data.mesage}
+                                </div>
+                            </div>
+                        ))}
                     </div>
+                ))}
 
-                    <div className="carousel-previous">
-                        <i className="fa fa-chevron-left p-2 p-2 pt-3 pb-3 fa-2x"></i>
-                    </div>
-                    <div className="carousel-next">
-                        <i className="fa fa-chevron-right p-2 pt-3 pb-3 fa-2x"/>
-                    </div>
-                </div>
-            ))}
+            </div>
+            <div className="carousel-previous" id="carousel-prev-btn">
+                <i className="fa fa-chevron-left p-2 p-2 pt-3 pb-3 fa-2x" />
+            </div>
+            <div className="carousel-next" id="carousel-next-btn">
+                <i className="fa fa-chevron-right p-2 pt-3 pb-3 fa-2x" />
+            </div>
         </React.Fragment>
     );
 }
--- src/components/carousel/slider-logic.js
+++ src/components/carousel/slider-logic.js
@@ -1,5 +1,56 @@
 
-export function sss() {
-  
+export function setTimer() {
+    var timer, numofSlides, currentslideindex = 0;
 
+    let slides = document.getElementsByClassName('carousel-image');
+    let next_btn = document.getElementById("carousel-next-btn")
+    let prev_btn = document.getElementById("carousel-prev-btn")
+
+    numofSlides = slides.length
+
+    function slide() {
+        let nextindex = currentslideindex + 1;
+        if (nextindex >= numofSlides) {
+            nextindex = 0
+        }
+
+        slides[currentslideindex].style.opacity = '0';
+        slides[nextindex].style.opacity = '1';
+
+        currentslideindex = nextindex;
+    }
+
+    function next() {
+        clearTimeout(timer);
+
+        let nextindex = currentslideindex + 1;
+        if (nextindex >= numofSlides) {
+            nextindex = 0
+        }
+
+        slides[currentslideindex].style.opacity = '0';
+        slides[nextindex].style.opacity = '1';
+
+        currentslideindex = nextindex;
+        timer = setInterval(slide, 4000);
+    }
+
+    function prev() {
+        clearTimeout(timer);
+
+        let previndex = currentslideindex - 1;
+        if (previndex < 0) {
+            previndex = numofSlides - 1;
+        }
+
+        slides[currentslideindex].style.opacity = '0'
+        slides[previndex].style.opacity = "1"
+
+        currentslideindex = previndex;
+        timer = setInterval(slide, 5000);
+    }
+
+    timer = setInterval(slide, 5000);
+    prev_btn.addEventListener('click', prev);
+    next_btn.addEventListener('click', next);
 }
